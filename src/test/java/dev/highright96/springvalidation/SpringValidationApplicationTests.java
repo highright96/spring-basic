636b9d6f01b6e73de9f4011b0fc35c76d509b8ac
Sangwoo96 <nsw5296@naver.com>
2021-05-04 01:29:00 +0900

    [init] 프로젝트 생성

---
+++ src/test/java/dev/highright96/springvalidation/SpringValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.highright96.springvalidation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
